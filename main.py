bbad73e8b069195bc754c33e2a36ada0e4605e15
tngeene <tngeene27@gmail.com>
2022-01-26 22:52:12 +0300

    refactored main.py to use class to enhance OOP

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ site_key = config('GOOGLE_CAPTCHA_KEY')
 driver_options = webdriver.ChromeOptions()
 driver_options.add_argument('--start_maximized')
 
-
 # * use 2captcha python client, read docs in https://pypi.org/project/2captcha-python/
 solver_config = {
     'apiKey': two_captcha_api_key,
@@ -29,71 +28,77 @@ solver_config = {
 
 solver = TwoCaptcha(**solver_config)
 
-def initiate_captcha_solver():
-    try:
-        print('Solving captcha...')
-        result = solver.recaptcha(sitekey=site_key, url=website_url)
-        # launch browser window upon successful 
-        # completion of captcha solving.
-        launch_selenium(result)
-    except ValidationException as e:
-        # invalid parameters passed
-        print(e)
-        return e
-    except NetworkException as e:
-        # network error occurred
-        print(e)
-        return e
-    except ApiException as e:
-        # api respond with error
-        print(e)
-        return e
-    except TimeoutException as e:
-        # captcha is not solved so far
-        print(e)
-        return e
 
+class SolveCaptcha:
+    
+    @classmethod 
+    def launch_selenium(cls, response):
+        if response:
+            print('Captcha Solved! Launching Browser...')
+            # initiate chrome webdriver
+            # * check webdrivers for firefox and others in https://selenium-python.readthedocs.io/installation.html#drivers
+            driver = webdriver.Chrome(service=Service(
+                ChromeDriverManager().install()),
+                                      options=driver_options)
 
-def launch_selenium(response):
-    if response:
-        print('Captcha Solved! Launching Browser...')
-        # initiate chrome webdriver 
-        # * check configs for firefox and others in https://selenium-python.readthedocs.io/installation.html#drivers
-        driver = webdriver.Chrome(service=Service(
-            ChromeDriverManager().install()),
-                                  options=driver_options)
+            # open browser window and navigate to Url
+            driver.get(website_url)
 
-        # open browser window and navigate to Url
-        driver.get(website_url)
+            driver.find_element(By.ID, 'name').send_keys('Ted')
+            driver.find_element(By.ID, 'phone').send_keys('000000000')
+            driver.find_element(By.ID,
+                                'email').send_keys('tngeene@captcha.com')
+            driver.find_element(By.ID,
+                                'comment-content').send_keys('test comment')
 
-        driver.find_element(By.ID, 'name').send_keys('Ted')
-        driver.find_element(By.ID, 'phone').send_keys('000000000')
-        driver.find_element(By.ID, 'email').send_keys('tngeene@captcha.com')
-        driver.find_element(By.ID, 'comment-content').send_keys('test comment')
+            google_captcha_response_input = driver.find_element(
+                By.ID, 'g-recaptcha-response')
 
-        google_captcha_response_input = driver.find_element(
-            By.ID, 'g-recaptcha-response')
+            # make input visible
+            driver.execute_script(
+                "arguments[0].setAttribute('style','type: text; visibility:visible;');",
+                google_captcha_response_input)
+            google_captcha_response_input.send_keys(response.get('code'))
 
-        # make input visible
-        driver.execute_script(
-            "arguments[0].setAttribute('style','type: text; visibility:visible;');",
-            google_captcha_response_input)
-        google_captcha_response_input.send_keys(response.get('code'))
+            driver.execute_script(
+                "arguments[0].setAttribute('style', 'display:none;');",
+                google_captcha_response_input)
+            # send text
+            driver.find_element(By.ID, 'send-message').click()
 
-        driver.execute_script(
-            "arguments[0].setAttribute('style', 'display:none;');",
-            google_captcha_response_input)
-        # send text
-        driver.find_element(By.ID, 'send-message').click()
+            sleep(randint(5, 10))
 
-        sleep(randint(5, 10))
+            driver.quit()
+            balance = solver.balance()
+            # show 2captcha Balance.
+            print(f'Your 2captcha balance is ${round(balance, 2)}')
+        else:
+            print('No response.')
 
-        driver.quit()
-        balance = solver.balance()
-        # show 2captcha Balance.
-        print(f'Your 2captcha balance is ${round(balance, 2)}')
-    print('Could not solve Captcha.')
+    def initiate_captcha_solver():
+        try:
+            print('Solving captcha...')
+            result = solver.recaptcha(sitekey=site_key, url=website_url)
+            # launch browser window upon successful
+            # completion of captcha solving.
+            SolveCaptcha.launch_selenium(result)
+        except ValidationException as e:
+            # invalid parameters passed
+            print(e)
+            return e
+        except NetworkException as e:
+            # network error occurred
+            print(e)
+            return e
+        except ApiException as e:
+            # api respond with error
+            print(e)
+            return e
+        except TimeoutException as e:
+            # captcha is not solved so far
+            print(e)
+            return e
 
 
 if __name__ == '__main__':
-    initiate_captcha_solver()
+    SolveCaptcha.initiate_captcha_solver()
